f3362e92cf03a4f264bd83fd79a9b30435628bd6
Nguyen Khanh <khanh@urbn8.com>
2022-09-05 18:56:43 +0700

    feat: build body part goals

--- src/analytics/test.py
+++ src/analytics/test.py
@@ -1,21 +1,31 @@
 from matplotlib import pyplot as plt
 # Example: https://matplotlib.org/stable/gallery/lines_bars_and_markers/barh.html
+import json
+import os
 
-# style
-plt.style.use('ggplot')
-plt.figure(figsize=(10, 16)) # size: https://stackabuse.com/change-figure-size-in-matplotlib/
+f = open(os.path.abspath('src/analytics/matches.json'))
+matches = json.load(f)
+
+totalLeft, totalRight, totalHead, totalOther = 0, 0, 0 , 0
+for match in matches:
+    totalLeft = totalLeft + match['left']
+    totalRight = totalRight + match['right']
+    totalHead = totalHead + match['head']
+    totalOther = totalOther + match['other']
+
+# set up data
+labels = 'Left foot (%d)' % totalLeft, 'Weak foot (%d)' % totalRight, 'Head (%d)' % totalHead, 'Others (%d)' % totalOther
+bodyPartGoals = (totalLeft, totalRight, totalHead, totalOther)
 
 
-# data
-categories = ["A", "Tán gái", "Tỏ tình", "Tâm trạng", "Nói lái ", "Rap quotes", "Cuộc sống", "Trả lời bá đạo", "Tình yêu", "18+", "Hư cấu", "Chưa phân loại", "Troll", "Ca dao tục, ngữ", "Chửi nghệ thuật", "FA", "Nhọ", "Ngược", "Bạn thừa biết", "Con gái", "Động lực", "Chia tay", "Sống chất", "Chuẩn CMNR", "Tái định nghĩa", "Gia đình bá đạo", "Ranh ngôn", "Thô nhưng thật", "Hôn nhân", "Học hành", "Hỏi ngu", "Bạn bè", "Sống ảo", "Nhan sắc", "Dê cụ ", "Bạn hỏi Admin trả lời", "Dân chơi", "Tuyển tập tên", "Tiền bạc", "Trào lưu", "Phũ", "Thành công", "Thầy cô", "Câu nói kinh điển", "Tôi", "Ơ ngờ u ", "Con trai - Con gái", "Lệ rơi", "Ăn uống", "Thi cử", "Ăn nhậu", "Tướng mạo", "Đàn ông", "Dân IT", "Giàu nghèo", "Tìm não lạc", "Nói lái 18+", "Fangirl", "Tin trong nước", "Nói lái tên ", "Game", "Nụ cười", "Đam mê", "Công sở", "Thời gian", "Ngủ", "Tết", "Táo Quân", "Cha mẹ", "Phía sau một chàng trai", "Quảng cáo", "Nhà người ta", "Làm quen với người lạ", "Hồi ký dân FA", "Yêu đơn phương", "Ngày phái đẹp", "Tâm sự cùng thiền sư", "Thư thả thính"]
-quantities = [335, 228, 198, 190, 188, 182, 180, 179, 171, 170, 170, 165, 157, 136, 135, 133, 133, 130, 127, 120, 119, 118, 117, 113, 113, 111, 108, 108, 108, 100, 98, 94, 93, 92, 89, 89, 87, 86, 86, 85, 82, 79, 78, 75, 73, 72, 69, 68, 67, 66, 65, 65, 64, 63, 61, 61, 60, 60, 60, 54, 50, 49, 49, 48, 45, 40, 39, 36, 35, 34, 32, 31, 30, 29, 29, 21, 17, 16]
+fig1, ax1 = plt.subplots()
 
-plt.barh(categories, quantities)
+ax1.set_title("Goals\nby parts of Body")
+ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 
-# info
-plt.title('Bài viết - Danh mục')
-plt.xlabel('Số lượng bài viết')
-plt.ylabel('Danh mục')
+wedges, texts, autotexts =ax1.pie(bodyPartGoals, labels=labels, autopct='%1.1f%%', startangle=-30)
 
-# plt.show()
-plt.savefig('foo.png')
+# store the images
+filename = 'body-part-goals.png'
+storedpath = os.path.join(os.path.abspath('frontend/img'), filename) 
+plt.savefig(storedpath)
