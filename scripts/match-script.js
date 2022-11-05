a6a8446e62f87df8c4892cc45b56ab38370345e7
Khank <nguyenngockhank@gmail.com>
2022-11-06 02:19:17 +0700

    chore: fetch new daat

---
+++ scripts/match-script.js
@@ -0,0 +1,60 @@
+const m20210502 = `04:38 trivela pass
+04:54 missed pen but still goal :> 
+06:49 last freekick goal of Barca (until now)`
+
+const m20180812 = `01:50 through pass 
+02:52 through pass 
+03:09 freekick 
+04:48 freekick hit the post then create chance
+07:45 freekick 
+08:06 nice pass
+08:49 freekick 
+09:52 through pass 
+10:34 freekick to assist =)) `
+
+const m20130818 = `00:59 1st goal ⚽️ 
+02:25 what stat cant show
+03:20 nice through ball =>  🅰️
+04:06 chip shot 
+05:45 2nd goal ⚽️ 
+`
+
+// -- s2010-2011
+const m20100821 = `03:30 ⚽️ - from a though pass - tap-in
+06:28 ⚽️ - tap-in
+09:17 tiki taka 
+11:20 ⚽️ - tap-in`
+
+const m20100829 = `00:16 ⚽️ - chip goal 
+01:40 pass
+02:50 almost goal 
+04:44 pass `
+
+const m20100919 = `01:58 through pass & hit the post
+02:11 ⚽️
+04:45 🅰️ from corner
+10:36 was fouled`
+
+const m20100929 = `02:24 trivela pass`
+
+const m20101020 = `02:39 ⚽️ - Outside the box
+05:06 pass through but offside
+05:33 offside goal 
+11:53 ⚽️ -  tap-in`;
+
+
+const matchScripts = {
+    m20210502,
+    m20180812,
+    m20130818,
+    // s10-11
+    m20100821,
+    m20100829,
+    m20100919,
+    m20100929,
+    m20101020,
+}
+
+module.exports = {
+    matchScripts
+}
